fb31e4bbfc24805b2cd6963c57410ddffe2bfff6
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-03-21 16:20:25 +0600

    Приложение получает список товаров в виде json-файла по api запросу

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,4 +1,20 @@
+require "net/http"
+
 class ProductController < ApplicationController
+
   def index
+    @products = JSON.parse(get_response_body)
+  end
+
+  private
+
+  def get_response_body
+    url = URI.parse('http://localhost:3000/api/products/index')
+    request = Net::HTTP::Get.new(url.to_s)
+    response = Net::HTTP.start(url.host, url.port) { |http|
+      http.request(request)
+    }
+
+    response.body
   end
 end
