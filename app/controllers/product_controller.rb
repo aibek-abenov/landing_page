a2ac438c5e2968099b061a23451d67e0e2c65f35
Алдияр Примбаев <primbayev.96aldi@gmail.com>
2019-03-21 18:51:33 +0600

    #75 поменял домен

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -9,7 +9,7 @@ class ProductController < ApplicationController
   private
 
   def get_response_body
-    url = URI.parse('http://localhost:3000/api/products/index')
+    url = URI.parse('http://ar-1-2-esdp-1.ltestl.com/api/products/index')
     request = Net::HTTP::Get.new(url.to_s)
     response = Net::HTTP.start(url.host, url.port) { |http|
       http.request(request)
